f20a28a5bb2f30fd64c820641750982b7b2a242c
Learning X <luc.st-pierre@protonmail.com>
2022-02-09 21:04:41 +0100

    generated a list of gentlemen component

--- src/components/Gentleman/Gentleman.js
+++ src/components/Gentleman/Gentleman.js
@@ -1,31 +1,31 @@
 const Gentleman = () => {
   return (
-    <li class="gentleman">
-      <div class="gentleman__avatar-container">
+    <li className="gentleman">
+      <div className="gentleman__avatar-container">
         <img
-          class="gentleman__avatar"
+          className="gentleman__avatar"
           src="img/fary.jpg"
           alt="The Fary pointing at you"
         />
-        <span class="gentleman__initial">F</span>
+        <span className="gentleman__initial">F</span>
       </div>
-      <div class="gentleman__data-container">
-        <h2 class="gentleman__name">The Fary</h2>
-        <ul class="gentleman__data-list">
-          <li class="gentleman__data">
-            <span class="gentleman__data-label">Profession:</span>
+      <div className="gentleman__data-container">
+        <h2 className="gentleman__name">The Fary</h2>
+        <ul className="gentleman__data-list">
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Profession:</span>
             Influencer
           </li>
-          <li class="gentleman__data">
-            <span class="gentleman__data-label">Status:</span> RIP
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Status:</span> RIP
           </li>
-          <li class="gentleman__data">
-            <span class="gentleman__data-label">Twitter:</span> Pending
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Twitter:</span> Pending
           </li>
         </ul>
       </div>
-      <i class="icon gentleman__icon fas fa-check"></i>
-      <i class="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+      <i className="icon gentleman__icon fas fa-check"></i>
+      <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
     </li>
   );
 };
